0546e877ea7898abf42f0f7cb777442114dab310
Vinandy <Vinandy4718@gmail.com>
2022-03-31 13:17:02 +0300

    New algorithm for ADD_NN_N

--- ADD_NN_N.py
+++ ADD_NN_N.py
@@ -2,10 +2,44 @@ import COM_NN_D
 
 
 def ADD_NN_N(a,b): #Функция N-4 Виноградов А.С.
-    D = COM_NN_D(a, b) #Алгоритм функции: Ищем сначало более длинную цифру, далее соединяем поэлементно при помощи zip
-    if D == 1 or D == 0:
-        slen = a
+    D = COM_NN_D(a, b) #Алгоритм функции:
+    mind = 0
+    if D == 0 or D == 1:
+        c1 = [0] * (len(a) + 1)
+        c2 = [0] * (len(a))
     else:
-        slen = b
-    c = [x + y for x, y in zip(a, b)] + slen[min(len(a), len(b)):]
-    return c
+        c1 = [0] * (len(b) + 1)
+        c2 = [0] * (len(b))
+    a.reverse()
+    b.reverse()
+    for i in range(len(b)):
+        if a[i]+b[i]<=9 and mind == 0:
+            c1[i] = a[i] + b[i]
+        elif a[i] + b[i] > 9 and mind == 0:
+            c1[i] = (a[i] + b[i])%10
+            mind = (a[i] + b[i])//10
+        elif mind == 1:
+            if a[i] + b[i] + mind <= 9:
+                c1[i] = a[i] + b[i] + mind
+                mind = 0
+            else:
+                c1[i] = (a[i] + b[i] + mind) % 10
+                mind = (a[i] + b[i] + mind) // 10
+    if mind != 0 and (D == 0 or D == 1):
+        c1[len(a)] = mind
+        c1.reverse()
+        return c1
+    elif mind != 0 and D == 2:
+        c1[len(b)] = mind
+        c1.reverse()
+        return c1
+    elif mind == 0 and (D == 0 or D == 1):
+        for i in range(len(a)):
+            c2[i] = c1[i]
+        c2.reverse()
+        return c2
+    elif mind == 0 and D == 2:
+        for i in range(len(b)):
+            c2[i] = c1[i]
+        c2.reverse()
+        return c2
