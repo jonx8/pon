3d4bc00bed2c3dbbae0aa4fddac72192ec66757f
IVAAN <63716313+IVAAN1357@users.noreply.github.com>
2022-03-31 11:23:00 +0300

    Added CON_NN_D and ADD_NN_N

---
+++ ADD_NN_N.py
@@ -0,0 +1,8 @@
+def ADD_NN_N(a,b): #Функция N-4 Виноградов А.С.
+    D = COM_NN_D(a, b) #Алгоритм функции: Ищем сначало более длинную цифру, далее соединяем поэлементно при помощи zip
+    if D == 1 or D == 0:
+        slen = a
+    else:
+        slen = b
+    c = [x + y for x, y in zip(a, b)] + slen[min(len(a), len(b)):]
+    return c
