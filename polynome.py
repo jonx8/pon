b60aab18eaa04385fa005b1a11863043634cb9c6
jonx8 <31346779+jonx8@users.noreply.github.com>
2022-04-04 21:07:34 +0300

    Update polynome.py

--- polynome.py
+++ polynome.py
@@ -12,11 +12,41 @@ class Polynome:
             self.C.append(Rational(i))  # массив с рац. числами
 
     def __str__(self):
-        """Возвращает строковое представление коэффициентов. Таланков Влад."""
-        str_out = []
-        for i in self.C:
-            str_out.append(str(i))
-        return ' '.join(str_out)
+        """Возвращает строковое представление многочлена. Малых Андрей."""
+        res = ''
+
+        def sign(num):
+            """Определяет, какой знак выводить перед членом. Малых Андрей"""
+            if num:
+                if num == '-':
+                    return ''
+                else:
+                    return '' if num.numer.b else '+'
+            return '+'
+
+        def deg(i):
+            """Принимает степень икса, возвращает строковое представление x^i. Малых Андрей"""
+            if i > 1:
+                return f'x^{i}'
+            elif i == 1:
+                return 'x'
+            return ''
+
+        for i in range(self.m, -1, -1):
+            j = self.m - i  # Номер коэффициента
+            koeff = self.C[j]   # Вид коэффициента при выводе
+
+            if koeff.numer.A == [1] and koeff.denom.A == [1] and i != 0:
+                koeff = '-' if koeff.numer.b else ''
+
+            if self.C[j].numer.A != [0]:
+                res += sign(koeff) + f'{koeff}{deg(i)}'
+
+        if not res:
+            return '0'
+        elif res[0] == '+':
+            res = res[1:]
+        return res
 
 
 def LED_P_Q(polynome):
