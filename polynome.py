df9d7e31651cb67edf67cd02529d8dc5b4ed6cde
jonx8 <31346779+jonx8@users.noreply.github.com>
2022-04-06 11:11:56 +0300

    Created ADD_PP_P, SUP_PP_P, MUL_PQ_P

--- polynome.py
+++ polynome.py
@@ -10,7 +10,15 @@ class Polynome:
         self.m = len(number) - 1  # степень многочлена
         for i in number:
             self.C.append(Rational(i))  # массив с рац. числами
-
+    
+    
+    def frontZerosDel(self):
+        """Удаление нулевых коэффициентов перед старшим членом. Малых Андрей"""
+        while self.C[0].numer.A == [0] and len(self.C) > 1:
+            self.C.pop(0)
+        self.m = len(self.C) - 1
+        
+        
     def __str__(self):
         """Возвращает строковое представление многочлена. Малых Андрей."""
 
@@ -80,6 +88,35 @@ def DER_P_P(a):
             a.C[i] = a.C[i]*Rational(str(t))
             t -= 1
     return a
+
+
+def ADD_PP_P(poly1, poly2):
+    """Сложение многочленов. Малых Андрей"""
+    if DEG_P_N(poly1) < DEG_P_N(poly2):
+        poly1, poly2 = poly2, poly1
+    for i in range(DEG_P_N(poly2) + 1):
+        poly1.C[i] = ADD_QQ_Q(poly1.C[i], poly2.C[i])
+    poly1.frontZerosDel()
+    return poly1
+
+
+def SUB_PP_P(poly1, poly2):
+    """Вычитание многочленов. Малых Андрей"""
+    if DEG_P_N(poly1) < DEG_P_N(poly2):
+        poly1, poly2 = poly2, poly1
+    for i in range(DEG_P_N(poly2) + 1):
+        poly1.C[i] = SUB_QQ_Q(poly1.C[i], poly2.C[i])
+    poly1.frontZerosDel()
+    return poly1
+
+
+def MUL_PQ_P(polynome, num):
+    """Умножение многочлена на число. Малых Андрей"""
+    for i in range(DEG_P_N(polynome) + 1):
+        polynome.C[i] = MUL_QQ_Q(polynome.C[i], num)
+    polynome.frontZerosDel()
+    return polynome
+
 
 if __name__ == '__main__':
     a = Polynome("-4/3 7/5 13/2 -5/3 2/4")
